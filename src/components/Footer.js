40ba808aa5c2e38e34267c578b6d4639ca2e5b8e
nizarafham <nizarafham17@gmail.com>
2025-05-08 19:52:08 +0700

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 // components/Footer.js
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
 import { FaInstagram, FaGithub, FaWhatsapp, FaLinkedin } from 'react-icons/fa';
@@ -6,12 +8,12 @@ import { FaInstagram, FaGithub, FaWhatsapp, FaLinkedin } from 'react-icons/fa';
 const Footer = () => {
 
   return (
-    <footer className="w-full px-4 lg:w-1/2 mx-auto py-8 text-xs text-opacity-100 opacity-100 text-gray-500 text-center border-t border-gray-200 flex flex-col items-center justify-center" style={{ backgroundColor: '#E4E0E1' }}>
-      <div className="mb-4 mr-70 md:mr-100">
+    <footer className="w-full px-4 lg:w-1/2 mx-auto py-8 text-xs text-gray-500 border-t border-gray-200 flex flex-col justify-center" style={{ backgroundColor: '#E4E0E1' }}>
+      <div className="mb-4 w-full flex flex-col items-start self-start">
         <Link href="/" className="font-bold text-3xl">
           code with nizar
         </Link>
-        <div className="flex space-x-4 mt-2">
+        <div className="flex space-x-4 mt-2 justify-start">
           <Link href="https://instagram.com/ny.zaru" target="_blank" rel="noopener noreferrer" className="hover:opacity-75">
             <FaInstagram size={30} />
           </Link>
@@ -26,7 +28,7 @@ const Footer = () => {
           </Link>
         </div>
       </div>
-      <div className="text-lg">
+      <div className="text-lg w-full text-left self-start">
         Copyright © 2025 code-with-nizar. All Rights Reserved
       </div>
     </footer>
